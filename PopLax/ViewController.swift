c5f9351b6384c60e20050d95b3c42e3261cf2590
Chibaibuki <budingmail@vip.qq.com>
2016-05-12 17:38:41 +0800

    fin the 3d touch and pop

--- PopLax/ViewController.swift
+++ PopLax/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import MetalPerformanceShaders
+import AudioToolbox
 
 class ViewController: UIViewController {
-
+    var finTheTouch = true;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var testPopView: UIImageView!
+    @IBOutlet weak var testPopButton: UIButton!
 
+    @IBAction func longPress(sender: UILongPressGestureRecognizer) {
+        self.testPopView.image = UIImage(named: "empty");
+    }
+    @IBAction func testPopDown(sender: AnyObject) {
+        self.testPopButton.setImage(UIImage(named: "empty"), forState: UIControlState.Normal);
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+//        finTheTouch = false;
+    }
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+//        print(touches);
+        finTheTouch = true;
+        
+    }
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let aTouch = touches.first;
+        if aTouch?.force > 5.0 && finTheTouch == true{
+            finTheTouch = false;
+            print("Hard");
+           AudioServicesPlaySystemSound(1520);
+        }
+    }
+
+    
+    
+    
 }
 
