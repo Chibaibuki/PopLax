cd69d37cf193561d5d3a47249330ffb073136ac2
Chibaibuki <budingmail@vip.qq.com>
2016-05-12 10:33:44 +0800

    Initial Commit

---
+++ PopLax/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PopLax
+//
+//  Created by 高向孚 on 5/12/16.
+//  Copyright © 2016 ByStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
